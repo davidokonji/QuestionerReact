1bb373106b16fbc571cf5508fc88f04c7449f0e2
davidokonji <davidokonji3@gmail.com>
2019-03-17 22:03:05 +0100

    Chore: setup eslint support - setup eslint and install necessary dependencies - [Finishes #164684477](https://www.pivotaltracker.com/story/show/164684477)

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,47 @@
 import path from 'path';
-import htmlWebpackPlugin from 'html-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 module.exports = {
-    entry: path.join(__dirname,'src','index.js'),
-    output: {
-        path: path.join(__dirname,'build'),
-        filename: 'index.bundle.js'
-    },
-    mode: process.env.NODE_ENV ||  'development',
-    resolve: {
-        modules: [path.resolve(__dirname,'src'), 'node_modules']
-    },
-    devServer:{
-        contentBase: path.join(__dirname,'src')
-    },
-    plugins: [
-        new htmlWebpackPlugin({
-            template: path.join(__dirname,'public','index.html')
-        })
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'index.bundle.js',
+  },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+  ],
+  // adding webpack babel support
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      }, {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      }, {
+        test: /\.(css|scss)$/,
+        use: ['style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(jpg|jpeg|mp3|png|gif|svg)$/,
+        loaders: ['file-loader'],
+      },
     ],
-    // adding webpack babel support 
-    module:{
-        rules:[{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            use: ['babel-loader']
-        },{
-            test: /\.(css|scss)$/,
-            use: ['style-loader',
-                    'css-loader',
-                    'sass-loader'
-        ]
-        },
-        {
-            test: /\.(jpg|jpeg|mp3|png|gif|svg)$/,
-            loaders: ['file-loader']
-        }
-    ]
-    }
+  },
 };
